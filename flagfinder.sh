7293d9cef3b229659f6e49b5196c14c11c026289
saturnhafen <saturnhafen@gmail.com>
2022-03-08 12:21:03 +0100

    Reordering

--- flagfinder.sh
+++ flagfinder.sh
@@ -5,11 +5,13 @@ ssh $1
 echo "Searching for 'flags/interesting_file' with content 'isec{.*}'"
 find / -type f -name flags/interesting_file -exec grep 'isec{.*}'  {} \;
 
-echo "Searching for 'flags/'"
-find / flags/
-
-echo "Searching for 'interesting_file'"
-find / --name interesting_file
-
 echo "Searching for file with content 'isec{.*}'"
-find -type f -exec grep 'isec{.*}'
+find -type f -exec grep 'isec{.*}'  {} \;
+
+if [[ $2 ]]; then
+  echo "Searching for 'flags/'"
+  find / flags/
+
+  echo "Searching for 'interesting_file'"
+  find / --name interesting_file
+fi
