b7c9f74a9cdb6d950f24b0e99911df05d00b3964
saturnhafen <saturnhafen@gmail.com>
2022-03-08 14:13:45 +0100

    fix flagfinder

--- flagfinder.sh
+++ flagfinder.sh
@@ -2,14 +2,16 @@
 
 # connect via ssh
 if [[ $1 ]]; then
-  ssh $1
-fi
+  ssh $1 "
 
-echo "Searching for 'flags/interesting_file' with content 'isec{.*}'"
+echo 'Searching for <flags/interesting_file> with content <isec{.*}>';
 find / -type f -name flags/interesting_file -exec grep 'isec{.*}'  {} \;
 
-echo "Searching for file with content 'isec{.*}'"
+echo 'Searching for file with content <isec{.*}>';
 find -type f -exec grep 'isec{.*}'  {} \;
 
-echo "Searching environment of current process"
-printenv | grep 'isec{.*}'
+echo 'Searching environment of current process';
+printenv | grep 'isec{.*}';
+"
+
+fi
