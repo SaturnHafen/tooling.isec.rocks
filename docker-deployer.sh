33fb9de3eb319125b362717555400b48dd275498
Alexander Sohn <github@asohn.de>
2022-03-10 13:48:26 +0100

    Add ripgrep and make safe for ipv6

--- docker-deployer.sh
+++ docker-deployer.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
+ssh_key="/home/$USER/.ssh/KEY"
+if [ "$#" -lt 3 ]; then
+    echo "[!] Usage $0 [username] [adress] [port]"
+    exit 1
+fi
 
+user=$1
+adress=$2
+port=$3
 
 # copy your public key to remote to allow key-login
 echo "adding your public key to the remote service..."
-ssh-copy-id -i ~/.ssh/id_rsa.pub $1
-
+ssh-copy-id -i $ssh_key -p "$port" "$user@$adress"
 # copy relevant files to remote maschine
-scp dockerchecker.sh $1:dockerchecker.sh
-scp flagfinder.sh $1:flagfinder.sh
+scp -i ~/.ssh/github -P "$port" flagfinder.sh dockerchecker.sh rg "$user@[$adress]:~"
 
-ssh $1 "chmod +x dockerchecker.sh; chmod +x flagfinder.sh; ./dockerchecker.sh"
+# Make files executable
+ssh "$user@$adress" -i ~/.ssh/github "chmod +x dockerchecker.sh; chmod +x flagfinder.sh; chmod +x rg"
+
+#Connect to machine
+ssh "$user@$adress" -i ~/.ssh/github
--- flagfinder.sh
+++ flagfinder.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 echo 'Searching environment of current process'
-printenv | grep -o 'isec{.*}'
+printenv | grep -H -s -i -o --color=always '.*isec{.*}'
 
 echo 'Searching environment of all processes'
-ps auxe | grep -o 'isec{.*}'
+ps auxe | grep -H -s -i -o --color=always '.*isec{.*}'
+
+echo 'ripgrep uses ALL paramters. Might be slow. Tweak to your needs.'
+echo 'Start ripgrep in /home/'
+./rg --follow --pretty --no-ignore --text --no-mmap --search-zip -j1 -o --hidden -i 'isec\{.*\}' /home/ 2>> error_log
+
+echo 'Strat ripgrep in /root/'
+./rg --follow --pretty --no-ignore --text --no-mmap --search-zip -j1 -o --hidden -i 'isec\{.*\}' /root/ 2>> error_log
+
+echo 'Start ripgrep in /'
+./rg --follow --pretty --no-ignore --text --no-mmap --search-zip -j1 -o --hidden -i -g '!/home/' -g '!/root/' 'isec\{.*\}' / 2>> error_log
 
 echo 'Searching for "interesting_file" with content "isec{.*}"'
-find / -type f -name interesting_file -exec grep 'isec{.*}'  {} \; 2>/dev/null
+find / -type f -name interesting_file -exec grep -H -s -i -o --color=always '.*isec{.*}'  {} \; 2>> error_log
 
 echo 'Searching for file with content "isec{.*}"'
-find / -type f -exec grep 'isec{.*}'  {} \; 2>/dev/null
+find / -type f -exec grep -H -s -i -o --color=always '.*isec{.*}'  {} \; 2>> error_log
